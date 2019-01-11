100fa90cbad8bfcd3f49ea748189538c4716d4fb
mehul0810 <mehul.gohil0810@gmail.com>
2019-01-11 18:16:42 +0530

    fix: rename file name and improve code formatting

--- includes/modules/class-perform-hide-wp-version.php
+++ includes/modules/class-perform-hide-wp-version.php
@@ -3,6 +3,10 @@
  * Perform Module - Hide WordPress Version.
  *
  * @since 1.0.0
+ *
+ * @package    Perform
+ * @subpackage Hide WordPress Version
+ * @author     Mehul Gohil
  */
 
 // Bail out, if accessed directly.
@@ -26,10 +30,10 @@ class Perform_Hide_WP_Version {
 	public function __construct() {
 		
 		// Remove WP Generator using action hook.
-		remove_action('wp_head', 'wp_generator');
+		remove_action( 'wp_head', 'wp_generator' );
 		
 		// Return empty string to the generator using filter hook.
-		add_filter('the_generator', __return_empty_string());
+		add_filter( 'the_generator', __return_empty_string() );
 		
 	}
 }
