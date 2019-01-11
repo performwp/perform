6aa87449a8206f2913ad95a21196b9580f4bfe6c
mehul0810 <mehul.gohil0810@gmail.com>
2019-01-11 17:57:35 +0530

    fix: load functions.php to load all the modules

--- perform-for-wp.php
+++ perform-for-wp.php
@@ -204,6 +204,7 @@ if ( ! class_exists( 'Perform' ) ) {
 			
 			require_once PERFORM_PLUGIN_DIR . '/includes/install.php';
 			require_once PERFORM_PLUGIN_DIR . '/includes/actions.php';
+			require_once PERFORM_PLUGIN_DIR . '/includes/functions.php';
 			require_once PERFORM_PLUGIN_DIR . '/includes/misc-functions.php';
 //			require_once PERFORM_PLUGIN_DIR . '/includes/assets-manager.php';
 //			require_once PERFORM_PLUGIN_DIR . '/includes/network.php';
