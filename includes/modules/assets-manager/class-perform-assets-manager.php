be4e6a9b7ebd2ccd60dc80c4587d951941e0aba7
mehul0810 <mehul.gohil0810@gmail.com>
2019-01-22 19:57:38 +0530

    feat: add assets manager file

---
+++ includes/modules/assets-manager/class-perform-assets-manager.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Perform - Assets Manager.
+ *
+ * @since 1.0.0
+ *
+ * @package    Perform
+ * @subpackage Assets Manager
+ * @author     Mehul Gohil
+ */
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Perform Assets Manager Class.
+ *
+ * @since 1.0.0
+ */
+class Perform_Assets_Manager {
+
+	public function __construct() {
+
+	}
+}
