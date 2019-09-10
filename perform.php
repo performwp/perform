7e6d788a899b3b8018c4359cf2a91e47151e43b1
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-09-10 15:04:15 +0530

    fix: update plugin header information for namespacing setup

--- perform.php
+++ perform.php
@@ -1,5 +1,20 @@
 <?php
+
 /**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and starts the plugin.
+ *
+ * @since      1.0.0
+ * @package    WordPress
+ * @subpackage Perform
+ * @author     Mehul Gohil
+ * @link       https://performwp.com
+ *
+ * @wordpress-plugin
+ *
  * Plugin Name: Perform - Performance Optimization Plugin for WordPress
  * Plugin URI: https://performwp.com/
  * Description: This plugin adds toolset for performance and speed improvements to your WordPress sites.
@@ -10,12 +25,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: perform
  * Domain Path: /languages
- *
- * @package    WordPress
- * @subpackage Perform
- * @author     Mehul Gohil
  */
-
 // Bailout, if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
