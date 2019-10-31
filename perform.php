f2cafeec06532154e5fdef54ee6834ec3ee419a2
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-10-31 11:41:00 +0530

    fix: remove namespacing code as of now

--- perform.php
+++ perform.php
@@ -26,26 +26,10 @@
  * Domain Path: /languages
  */
 
-namespace Perform;
-
 // Bailout, if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-/**
- * Define necessary configuration files.
- */
-require_once __DIR__ . '/config/constants.php';
-
-/**
- * Automatically loads files used throughout the plugin.
- */
-require_once __DIR__ . '/autoloader.php';
-
-// Initialize the plugin.
-$plugin = new Includes\Plugin();
-$plugin->register();
 
 /**
  * Check for class Perform existence.
