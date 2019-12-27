ee856732258d5d5dda38c0f6400d252a7ffba191
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-12-27 11:19:51 +0530

    chore: update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,41 @@ const path = require( 'path' );
 const webpack = require( 'webpack' );
 const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
-const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' );
 const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
 const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
 const WebpackRTLPlugin = require( 'webpack-rtl-plugin' );
+const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
 const wpPot = require( 'wp-pot' );
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const inProduction = ( 'production' === process.env.NODE_ENV );
+const mode = inProduction ? 'production' : 'development';
 
 const config = {
-
-	devtool: 'source-map',
+	mode,
+	entry: {
+		perform: [ './assets/src/scss/frontend/main.scss', './assets/src/js/frontend/main.js' ],
+		admin: [ './assets/src/scss/admin/admin.scss' ],
+	},
+	output: {
+		path: path.join( __dirname, './assets/dist/' ),
+		filename: 'js/[name].js',
+	},
+	// Ensure modules like magnific know jQuery is external (loaded via WP).
+	externals: {
+		$: 'jQuery',
+		jquery: 'jQuery',
+		lodash: 'lodash',
+	},
+	devtool: ! inProduction ? 'source-map' : '',
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				test: /\.js(\?.*)?$/i,
+				sourceMap: true,
+			}),
+		],
+	},
 	module: {
 		rules: [
 
@@ -28,31 +52,49 @@ const config = {
 			// Create RTL styles.
 			{
 				test: /\.css$/,
-				loader: ExtractTextPlugin.extract( 'style-loader' ),
+				use: [
+					MiniCSSExtractPlugin.loader,
+					{
+						loader: 'style-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
 			},
 
 			// SASS to CSS.
 			{
 				test: /\.scss$/,
-				use: ExtractTextPlugin.extract( {
-					use: [ {
+				use: [
+					MiniCSSExtractPlugin.loader,
+					{
 						loader: 'css-loader',
 						options: {
 							sourceMap: true,
 						},
-					}, {
-						loader: 'postcss-loader',
-						options: {
-							sourceMap: true,
-						},
-					}, {
+					},
+					{
 						loader: 'sass-loader',
 						options: {
 							sourceMap: true,
 							outputStyle: ( inProduction ? 'compressed' : 'nested' ),
 						},
 					} ],
-				} ),
+			},
+
+			// Font files.
+			{
+				test: /\.(ttf|otf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'fonts/[name].[ext]',
+							publicPath: '../',
+						},
+					},
+				],
 			},
 
 			// Image files.
@@ -79,8 +121,9 @@ const config = {
 
 		new ExtractTextPlugin( 'css/[name].css' ),
 
-		// Create RTL css.
-		new WebpackRTLPlugin(),
+		new MiniCSSExtractPlugin( {
+			filename: 'css/[name].css',
+		} ),
 
 		// Copy images and SVGs
 		new CopyWebpackPlugin( [ { from: 'assets/src/images', to: 'images' } ] ),
@@ -91,23 +134,19 @@ const config = {
 	],
 };
 
-module.exports = [
-	Object.assign( {
-		entry: {
-			perform: [ './assets/src/scss/frontend/main.scss', './assets/src/js/frontend/main.js' ],
-			admin: [ './assets/src/scss/admin/admin.scss' ],
-		},
-		output: {
-			path: path.join( __dirname, './assets/dist/' ),
-			filename: 'js/[name].js',
-			library: 'Perform',
-			libraryTarget: 'umd',
-		},
-	}, config ),
-];
-
 // inProd?
 if ( inProduction ) {
+
+	// Create RTL css.
+	config.plugins.push( new WebpackRTLPlugin( {
+		suffix: '-rtl',
+		minify: true,
+	} ) );
+
+	// Minify images.
+	// Must go after CopyWebpackPlugin above: https://github.com/Klathmon/imagemin-webpack-plugin#example-usage
+	config.plugins.push( new ImageminPlugin( { test: /\.(jpe?g|png|gif|svg)$/i } ) );
+
 	// POT file.
 	wpPot( {
 		package: 'Perform for WordPress',
@@ -119,8 +158,10 @@ if ( inProduction ) {
 	} );
 
 	// Uglify JS.
-	config.plugins.push( new webpack.optimize.UglifyJsPlugin( { sourceMap: true } ) );
+	// config.plugins.push( new webpack.optimize.UglifyJsPlugin( { sourceMap: true } ) );
 
 	// Minify CSS.
-	config.plugins.push( new webpack.LoaderOptionsPlugin( { minimize: true } ) );
+	// config.plugins.push( new webpack.LoaderOptionsPlugin( { minimize: true } ) );
 }
+
+module.exports = config;
