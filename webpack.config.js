ff70e36958a8e25cd647b6efafdeb7861e31b2c3
Mehul Gohil <mehul.gohil@get10up.com>
2024-11-13 15:31:59 +0530

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-const path = require( 'path' );
+const path = require('path');
 const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
-const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
 const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
-const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
+const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
+const WebpackRTLPlugin = require( 'webpack-rtl-plugin' );
 const wpPot = require( 'wp-pot' );
 
 const inProduction = ( 'production' === process.env.NODE_ENV );
@@ -11,23 +12,15 @@ const mode = inProduction ? 'production' : 'development';
 const config = {
 	mode,
 	entry: {
-		perform: [ './assets/src/scss/frontend/main.scss', './assets/src/js/frontend/main.js' ],
-		admin: [ './assets/src/scss/admin/admin.scss'],
+		perform: [ './assets/src/js/frontend/main.js', './assets/src/scss/frontend/main.scss'],
+		admin: [ './assets/src/scss/admin/admin.scss' ],
 	},
 	output: {
-		path: path.join( __dirname, './assets/dist/' ),
-		filename: 'js/[name].js',
+		path: path.join(__dirname, 'assets/dist/'),
+		filename: 'js/[name].min.js',
 	},
 	module: {
 		rules: [
-
-			// Use Babel to compile JS.
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-			},
-
 			// Create RTL styles.
 			{
 				test: /\.css$/,
@@ -52,9 +45,6 @@ const config = {
 						loader: 'sass-loader',
 						options: {
 							sourceMap: true,
-							sassOptions: {
-								outputStyle: ( inProduction ? 'compressed' : 'nested' ),
-							}
 						},
 					} ],
 			},
@@ -88,15 +78,16 @@ const config = {
 			},
 		],
 	},
-
-	// Plugins. Gotta have em'.
 	plugins: [
 		// Removes the "dist" folder before building.
-		new CleanWebpackPlugin( [ 'assets/dist' ] ),
+		new CleanWebpackPlugin({
+			cleanOnceBeforeBuildPatterns: [ 'assets/dist' ]
+		}),
 
 		new MiniCSSExtractPlugin( {
 			filename: 'css/[name].css',
 		} ),
+
 		new CopyWebpackPlugin( {
 			patterns: [
 				{
@@ -109,6 +100,12 @@ const config = {
 };
 
 if ( inProduction ) {
+	// Create RTL css.
+	config.plugins.push( new WebpackRTLPlugin( {
+		suffix: '-rtl',
+		minify: true,
+	} ) );
+
 	// Minify images.
 	// Must go after CopyWebpackPlugin above: https://github.com/Klathmon/imagemin-webpack-plugin#example-usage
 	config.plugins.push( new ImageminPlugin( { test: /\.(jpe?g|png|gif|svg)$/i } ) );
@@ -121,7 +118,7 @@ if ( inProduction ) {
 		relativeTo: './',
 		src: [ './**/*.php', '!./includes/libraries/**/*', '!./vendor/**/*' ],
 		bugReport: 'https://github.com/mehul0810/perform/issues/new',
-		team: 'Mehul Gohil <hello@mehulgohil.com>',
+		team: 'PerformWP <hello@performwp.com>',
 	} );
 }
 
