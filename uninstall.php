d5ac934d333e1f00212ba5be37eb17e4487ec763
Mehul Gohil <mehul.gohil@get10up.com>
2025-04-26 16:57:53 +0530

    fix issue with uninstall of the plugin

--- uninstall.php
+++ uninstall.php
@@ -1,26 +1,22 @@
 <?php
 /**
- * Uninstall.
+ * Plugin Uninstall Handler.
  *
  * @since 1.0.0
- *
- * @package    Perform
+ * @package Perform
  * @subpackage Uninstall
- * @author     Mehul Gohil
  */
 
-// Bailout, if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
 /**
- * Process Uninstall.
+ * Handle Perform plugin uninstall process.
  *
  * @since 1.0.0
  */
-function perform_uninstall() {
-
+function perform_handle_plugin_uninstall() {
 
 	$setting_types = array(
 		'perform_common',
@@ -29,40 +25,34 @@ function perform_uninstall() {
 		'perform_woocommerce',
 		'perform_advanced',
 		'perform_import_export',
-		'perform_support'
+		'perform_support',
 	);
 
-	$remove_data_on_uninstall = perform_get_option( 'remove_data_on_uninstall', 'perform_advanced' );
+	if ( function_exists( 'perform_get_option' ) ) {
+		$remove_data_on_uninstall = perform_get_option( 'remove_data_on_uninstall', 'perform_advanced' );
+	} else {
+		$remove_data_on_uninstall = true; // Default to true on uninstall if not accessible.
+	}
+
 	if ( $remove_data_on_uninstall ) {
 
 		if ( is_multisite() ) {
+			$sites = get_sites( array( 'deleted' => 0 ) );
 
-			// Get sites list.
-			$sites = array_map(
-				'get_object_vars',
-				get_sites( array(
-					'deleted' => 0
-				) )
-			);
-
-			// Loop through each site and delete all the setting types.
-			if ( is_array( $sites ) && count( $sites ) > 0 ) {
+			if ( ! empty( $sites ) ) {
 				foreach ( $sites as $site ) {
 					foreach ( $setting_types as $option ) {
-						delete_blog_option( $site['blog_id'], $option );
+						delete_blog_option( (int) $site->blog_id, $option );
 					}
 				}
 			}
 		} else {
-
-			// Loop through the setting types to delete.
 			foreach ( $setting_types as $option ) {
 				delete_option( $option );
 			}
 		}
 	}
-
-
 }
 
-register_uninstall_hook( PERFORM_PLUGIN_FILE, 'perform_uninstall' );
+// Directly run the uninstall handler without needing constants.
+perform_handle_plugin_uninstall();
