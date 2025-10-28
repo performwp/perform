7eb16fa9bf59c92351f9cd531b7cdfd423171387
Mehul Gohil <mehul.gohil@get10up.com>
2025-10-29 00:00:22 +0530

    remove legacy settings section

--- src/Admin/Settings/Menu.php
+++ src/Admin/Settings/Menu.php
@@ -37,7 +37,6 @@ class Menu extends Api {
 		$this->tabs   = Helpers::get_settings_tabs();
 
 		add_action( 'admin_menu', [ $this, 'register_admin_menu' ], 9 );
-		add_action( 'in_admin_header', [ $this, 'render_settings_page_header' ] );
 		add_action( 'wp_ajax_perform_save_settings', [ $this, 'save_settings' ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_admin_assets' ] ); // Added for React settings localization
 	}
@@ -61,59 +60,6 @@ class Menu extends Api {
 	}
 
 	/**
-	 * Render Settings Page Header.
-	 *
-	 * @since  1.4.0
-	 * @access public
-	 *
-	 * @return void
-	 */
-	public function render_settings_page_header() {
-		$screen = get_current_screen();
-
-		if ( 'settings_page_perform_settings' !== $screen->id ) {
-			return;
-		}
-		?>
-		<div id="perform-settings-page" class="perform-settings-page"></div>
-		<div class="perform-dashboard-header">
-			<div class="perform-dashboard-header-title">
-				<img src="<?php echo esc_url( PERFORM_PLUGIN_URL . 'assets/dist/images/logo.png' ); ?>" alt="<?php esc_attr_e( 'PerformWP', 'perform' ); ?>"/>
-			</div>
-			<?php $this->render_header_navigation(); ?>
-		</div>
-		<?php
-	}
-
-	/**
-	 * Render Header Navigation.
-	 *
-	 * @since  1.4.0
-	 * @access public
-	 *
-	 * @return void
-	 */
-	public function render_header_navigation() {
-		$current_tab = Helpers::get_current_tab();
-		$tabs        = apply_filters( 'perform_settings_navigation_tabs', $this->tabs );
-
-		if ( count( $tabs ) === 1 ) {
-			return;
-		}
-		?>
-		<ul class="perform-header-navigation">
-			<?php foreach ( $tabs as $slug => $tab ) : ?>
-				<li>
-					<a href="<?php echo esc_url( admin_url( 'options-general.php?page=perform_settings&tab=' . $slug ) ); ?>" class="<?php echo esc_attr( $slug === $current_tab ? 'active' : '' ); ?>">
-						<?php echo esc_html( $tab ); ?>
-					</a>
-				</li>
-			<?php endforeach; ?>
-		</ul>
-		<?php
-	}
-
-	/**
 	 * Render Settings Page.
 	 *
 	 * @since  1.0.0
@@ -122,56 +68,15 @@ class Menu extends Api {
 	 * @return void
 	 */
 	public function render_settings_page() {
+		$screen = get_current_screen();
+
+		// Only render on the Perform settings page.
+		if ( 'settings_page_perform_settings' !== $screen->id ) {
+			return;
+		}
 		?>
-		<div class="wrap perform-admin-settings-wrap">
-			<div class="perform-admin-settings--left-section">
-				<?php $this->render_left_section(); ?>
-			</div>
-			<div class="perform-admin-settings--right-section">
-				<?php $this->render_right_section(); ?>
-			</div>
-		</div>
+		<div id="perform-settings-page" class="perform-settings-page"></div>
 		<?php
-	}
-
-	/**
-	 * Render `Left Section` of the admin settings.
-	 *
-	 * This section will display the admin settings fields.
-	 *
-	 * @since  2.0.0
-	 * @access public
-	 *
-	 * @return void
-	 */
-	public function render_left_section() {
-		$fields = Helpers::get_settings_fields();
-
-		$this->render_fields( $fields );
-	}
-
-	/**
-	 * Render `Right Section` of the admin settings.
-	 *
-	 * This section will display the help area to ensure better utilizing of the plugin.
-	 *
-	 * @since  2.0.0
-	 * @access public
-	 *
-	 * @return void
-	 */
-	public function render_right_section() {
-		ob_start();
-
-		$check_our_documentation = esc_html__( 'Check our documentation', 'perform' );
-		?>
-		<div class="performwp-sidebar-section">
-			<a href="https://performwp.com/docs/" title="<?php echo $check_our_documentation; ?>" target="_blank">
-				<img src="<?php echo PERFORM_PLUGIN_URL . 'assets/dist/images/check-our-documentation.svg'; ?>" alt="<?php echo $check_our_documentation; ?>"/>
-			</a>
-		</div>
-		<?php
-		echo ob_get_clean();
 	}
 
 	/**
